563008356fc2c79d03b0f09d44a738529e07dcd8
Christiane Horschke <christiane.horschke@lug-ag.de>
2021-07-21 12:05:23 +0200

    Projektdateien hinzufügen.

---
+++ Übung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
